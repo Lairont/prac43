48f4336eda6293446c377021a0cbcc1459d9213e
Никита <153456515+Lairont@users.noreply.github.com>
2025-05-23 11:31:52 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        struct Notebook
+        {
+            public string Model;
+            public string Brand;
+            public decimal Price;
+
+            public Notebook(string model, string brand, decimal price)
+            {
+                Model = model;
+                Brand = brand;
+                Price = price;
+
+            }
+            public void DisplayInfo()
+            {
+                Console.WriteLine("\nИнформация о ноутбуке:");
+                Console.WriteLine($"Модель: {Model}");
+                Console.WriteLine($"Производитель: {Brand}");
+                Console.WriteLine($"Цена: {Price}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите модель ноутбука: ");
+            string model = Console.ReadLine();
+            Console.Write("Введите производителя: ");
+            string brand = Console.ReadLine();
+            Console.Write("Введите цену: ");
+            decimal price;
+
+            while (!decimal.TryParse(Console.ReadLine(), out price))
+            {
+                Console.Write("Ошибка! Введите числовое значение цены: ");
+            }
+
+            Notebook notebook = new Notebook(model, brand, price);
+
+            notebook.DisplayInfo();
+
+            Console.Read();
+
+        }
+    }
+}
