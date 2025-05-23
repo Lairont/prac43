96f8e9fff9aafb7d91e4e1598b00a281340bfebd
Никита <153456515+Lairont@users.noreply.github.com>
2025-05-23 12:51:35 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    //Создать структуру для хранения информации о студентах, включающую поля для ФИО, группы и оценок по таким предметам, как информатика, физика и история.
+    //Реализовать возможность ввода данных с клавиатуры, сохранение их в коллекции, а затем выводить сведения об объектах в табличном виде. Если объекты не были введены, вывести сообщение об их отсутствии.
+    //Также нужно вычислить средний балл по каждому предмету и вывести информацию о студентах, у которых средний балл выше 4, подсчитав их количество.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Кол-во студентов: ");
+            int n = int.Parse(Console.ReadLine());
+            Student[] students = new Student[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"\nВвод данных для студента #{i + 1}");
+                students[i] = Student.Print();
+            }
+            Console.WriteLine(
+        }
+    }
+}
---
+++ ConsoleApp2/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    struct Student
+    {
+        public string FIO;
+        public string Group;
+        public int Informatics;
+        public int Physics;
+        public int History;
+
+        public Student(string FIO, string Group, int Informatics, int Physics, int History)
+        {
+            this.FIO = FIO;
+            this.Group = Group;
+            this.Informatics = Informatics;
+            this.Physics = Physics;
+            this.History = History;
+        }
+
+        public static Student Print()
+        {
+            Console.WriteLine("ФИО: ");
+            string FIO = Console.ReadLine();
+            Console.WriteLine("Группа: ");
+            string Group = Console.ReadLine();
+            Console.WriteLine("Оценка по Информатике: ");
+            int Information = int.Parse(Console.ReadLine());
+            Console.WriteLine("Оценка по Физике: ");
+            int Physics = int.Parse(Console.ReadLine());
+            Console.WriteLine("Оценка по Истории: ");
+            int History = int.Parse(Console.ReadLine());
+            return new Student(FIO, Group, Information, Physics, History);
+        }
+        public override string ToString()
+        {
+            return $"ФИО: {FIO}\nГруппа: {Group}\nОценка по Информатике: {Informatics}\nОценка по Физике: {Physics}\nОценка по Истории: {History}";
+        }
+        public double SredniuBall()
+        {
+            return (Informatics + Physics + History) / 3;
+        }
+    }
+}
