a8c3362094fccd75209ee811243c576f90e58ad5
Никита <153456515+Lairont@users.noreply.github.com>
2025-05-28 13:31:18 +0300

    Add files via upload

---
+++ ConsoleApp8/Notebook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal struct Notebook
+    {
+        public string model;
+        public string manufacturer;
+        public int price;
+
+        public Notebook(string model, string manufacturer, int price)
+        {
+            this.model = model;
+            this.manufacturer = manufacturer;
+            this.price = price;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Модель: {model}\nПроизводитель: {manufacturer}\nЦена: {price}");
+        }
+    }
+}
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook notebook = new Notebook("Макбук", "Эппл", 150000);
+            notebook.Info();
+
+            Console.ReadLine();
+        }
+    }
+}
