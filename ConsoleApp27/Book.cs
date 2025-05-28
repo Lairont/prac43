0a9d3c5cf7a7da516635f0f31930cc6058179335
Никита <153456515+Lairont@users.noreply.github.com>
2025-05-28 19:52:28 +0300

    Add files via upload

---
+++ ConsoleApp27/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    internal struct Book
+    {
+        public string title;
+        public string author;
+        public int year;
+        public int pages;
+
+        public Book(string title, string author, int year, int pages)
+        {
+            this.title = title;
+            this.author = author;
+            this.year = year;
+            this.pages = pages;
+        }
+
+        public static Book Print()
+        {
+            Console.Write("Название: ");
+            string title = Console.ReadLine();
+            Console.Write("Автор: ");
+            string author = Console.ReadLine();
+            Console.Write("Год издания: ");
+            int year = int.Parse(Console.ReadLine());
+            Console.Write("Кол-во страниц: ");
+            int pages = int.Parse(Console.ReadLine());
+
+            return new Book(title, author, year, pages);
+        }
+        public override string ToString()
+        {
+            return $"Название: {title}\nАвтор: {author}\nГод издания: {year}\nКоличество страниц: {pages}";
+        }
+    }
+}
---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Кол-во книг: ");
+            int n = int.Parse(Console.ReadLine());
+            Book[] books = new Book[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"\nКнига {i + 1}:");
+                books[i] = Book.Print(); 
+            }
+            Console.WriteLine();
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book);
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
